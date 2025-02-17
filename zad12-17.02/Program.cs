ca9f39cd7915ecb225510b619a8dfb7d96efd4d6
MEGA <MEGA@gmail.com>
2025-02-17 17:39:26 +0200

    Add project files.

---
+++ zad12-17.02/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad12_17._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students =
+            {
+             new Student("John", 20),
+             new Student("Jane", 19),
+             new Student("Alice", 24),
+             new Student("Bob", 23)
+            };
+
+             
+
+            foreach(var student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.WriteLine();
+
+            BubbleSort(students);
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+
+        }
+
+        static void BubbleSort(Student[] students)
+        {
+            int n = students.Length;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (students[j].Age > students[j + 1].Age)
+                        (students[j], students[j + 1]) = (students[j + 1], students[j]);
+
+        }
+    }
+}
---
+++ zad12-17.02/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad12_17._02
+{
+    internal class Student
+    {
+        //new int[] ages = new int[4];
+        public string Name { get; set; }
+        public int Age { get; set; }
+      
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Age} years old";
+        }
+    }
+}
